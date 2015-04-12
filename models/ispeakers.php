ed92c0216c52376cecc6acec9bbc999f26ee3d4e
Yen-chi Chen <mark86092@gmail.com>
2015-04-13 03:33:24 +0800

    Update

--- models/ispeakers.php
+++ models/ispeakers.php
@@ -18,4 +18,23 @@ class ISpeakers {
 		$conn->close();
 		return $data;
 	}
+	function get($conn, $id) {
+		$stmt = $conn->prepare("SELECT `name`, `email`, `title`, `slide_file` FROM `ispeakers` WHERE `id` = ?");
+		$stmt->bind_param('s', $id);
+		$stmt->execute();
+		$stmt->bind_result($name, $email, $title, $slide_file);
+		$data = null;
+		if ($stmt->fetch()) {
+			$data = array(
+				'name' => $name,
+				'email' => $email,
+				'title' => $title,
+				'slide_file' => $slide_file
+			);
+		}
+
+		$stmt->close();
+		$conn->close();
+		return $data;
+	}
 }
