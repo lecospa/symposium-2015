9ae621b7a360e2d4c2c3a19abc31d3eabe7a66bd
Yen-chi Chen <mark86092@gmail.com>
2015-04-13 04:39:32 +0800

    fix

--- registration.php
+++ registration.php
@@ -1,12 +1,9 @@
 <?php
+require_once('init.php');
 if ($_POST['submit'] == 'Submit') {
 	try {
 	} catch (RuntimeException $e) {
 	}
 } else {
-	require_once('libs/Smarty.class.php');
-	$smarty = new Smarty;
-
-	$smarty->caching = true;
 	$smarty->display('registration.html');
 }
