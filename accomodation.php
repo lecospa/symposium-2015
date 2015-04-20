1496c395612ebdd2d70eb75000848383bd332c35
Yen-chi Chen <mark86092@gmail.com>
2015-04-20 15:53:36 +0800

    Update accommodation

--- accomodation.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('init.php');
-class Accomodation extends View {
-	function get() {
-		$this->smarty->assign('scope', __CLASS__);
-		$this->smarty->display('accomodation.html');
-	}
-}
-new Accomodation;
