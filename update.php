7813ef7d880d4bc7e272212abf800a661250f7eb
Yen-chi Chen <mark86092@gmail.com>
2015-04-20 15:25:17 +0800

    update update.php

--- update.php
+++ update.php
@@ -1,5 +1,7 @@
 <?php
-exec('git pull origin master', $output);
-foreach ($output as $line) {
-	echo $line."\n";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	exec('git pull origin master', $output);
+	foreach ($output as $line) {
+		echo $line."\n";
+	}
 }
