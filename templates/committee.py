8dc1d82fc0593feea3e837c17177d04979ef6032
Yen-chi Chen <mark86092@gmail.com>
2015-05-26 05:57:41 +0800

    fix email address error

--- templates/committee.py
+++ templates/committee.py
@@ -13,5 +13,5 @@ for x in s:
     print('<div class="thumbnail"><img src="img/committee/{}.JPG" class="img-responsive img-circle" style="height: 150px;">'.format(x[0]))
     print('<h3>{}</h3>'.format(x[0]))
     if len(x) == 2:
-        print('<a href="{}">{}</a>'.format(x[1], x[1]))
+        print('{}'.format(x[1].replace("@", "[AT]")))
     print('</div></div>')
