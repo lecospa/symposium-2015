b77836b3efb3a788ea1d12e2d05c94baef8d6ccc
Yen-chi Chen <mark86092@gmail.com>
2015-06-02 16:56:05 +0800

    update committee

--- templates/committee.py
+++ templates/committee.py
@@ -13,5 +13,6 @@ for x in s:
     print('<div class="thumbnail"><img src="img/committee/{}.JPG" class="img-responsive img-circle" style="height: 150px;">'.format(x[0]))
     print('<h3>{}</h3>'.format(x[0]))
     if len(x) == 2:
-        print('{}'.format(x[1].replace("@", "[AT]")))
+        #print('{}'.format(x[1].replace("@", "[AT]")))
+        print('{}'.format(x[1]))
     print('</div></div>')
