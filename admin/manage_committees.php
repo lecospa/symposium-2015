042397be25322f950e5cabaa63a0bc3d8dc6144a
Yen-chi Chen <mark86092@gmail.com>
2015-08-21 01:15:26 +0800

    update admin

---
+++ admin/manage_committees.php
@@ -0,0 +1,28 @@
+<?php
+namespace Admin;
+require_once('../init.php');
+
+class Index extends \View {
+	public function get() {
+		$token = $_GET['token'];
+		$auth = \Models\Auth::get(\db::get(), $token);
+		if ($auth['scope'] == 'sudo') {
+			$conn = \db::get();
+			$iac_chairs = \Models\People::all_with_token_by_type($conn, 'IACCHAIR');
+			$iacs = \Models\People::all_with_token_by_type($conn, 'IAC');
+			$loc_chairs = \Models\People::all_with_token_by_type($conn, 'LOCCHAIR');
+			$locs = \Models\People::all_with_token_by_type($conn, 'LOC');
+			$conn->close();
+
+			$this->smarty->assign('iac_chairs', $iac_chairs);
+			$this->smarty->assign('iacs', $iacs);
+			$this->smarty->assign('loc_chairs', $loc_chairs);
+			$this->smarty->assign('locs', $locs);
+			$this->smarty->assign('token', $token);
+			$this->smarty->display('admin/manage_committees.html');
+		} else {
+			header('Location: ' . TOP);
+		}
+	}
+}
+new Index;
