06a05b621e47fde7dcb37a4e719cd98f6acc284a
Yen-chi Chen <mark86092@gmail.com>
2015-08-24 14:16:11 +0800

    update

--- admin/index.php
+++ admin/index.php
@@ -9,10 +9,14 @@ class Index extends \View {
 		if ($auth['scope'] == 'sudo') {
 			$conn = \db::get();
 			$plenary_speakers = \Models\People::all_with_token_by_type($conn, 'Plenary');
+			$parallel_speakers = \Models\People::all_with_token_by_type($conn, 'Parallel');
+			$poster_speakers = \Models\People::all_with_token_by_type($conn, 'Poster');
 			$attendees = \Models\People::all_with_token_by_type($conn, 'Normal');
 			$conn->close();
 
 			$this->smarty->assign('plenary_speakers', $plenary_speakers);
+			$this->smarty->assign('parallel_speakers', $parallel_speakers);
+			$this->smarty->assign('poster_speakers', $poster_speakers);
 			$this->smarty->assign('attendees', $attendees);
 			$this->smarty->assign('token', $token);
 			$this->smarty->display('admin/index.html');
--- submission.php
+++ submission.php
@@ -2,8 +2,52 @@
 require_once('init.php');
 class Submission extends View {
 	function get() {
-		$this->smarty->assign('scope', __CLASS__);
-		$this->smarty->display('submission.html');
+		$token = $_GET['token'];
+		$auth = \Models\Auth::get(\db::get(), $token);
+
+		if ($auth['scope'] == 'people_an') {
+			$this->smarty->assign('scope', __CLASS__);
+			$this->smarty->assign('token', $token);
+			$this->smarty->display('submission_creation.html');
+		} else {
+			$this->smarty->assign('scope', __CLASS__);
+			$this->smarty->display('submission.html');
+		}
+	}
+	function post() {
+		$token = $_GET['token'];
+		$conn = \db::get();
+		$auth = \Models\Auth::get($conn, $token);
+
+		if ($auth['scope'] == 'people_an') {
+			$type = $_POST['type'];
+			$email = $_POST['email'];
+			$first_name = $_POST['first_name'];
+			$last_name = $_POST['last_name'];
+			if (empty($first_name)) {
+				header('Location: ' . TOP . 'admin/index.php?token='.$token);
+				return;
+			}
+			try {
+				$_id = \Models\People::insert($conn, $type, $first_name, $last_name, $email);
+			} catch (\Exception $e) {
+				echo $e->getMessage();
+			}
+			$_token = self::generatorPassword(8);
+			
+			\Models\Auth::insert($conn, 'people', $_id, $_token);
+			$conn->close();
+			header('Location: ' . TOP . 'person/main.php?token='.$_token);
+		}
+	}
+	private function generatorPassword($password_len) {
+		$password = '';
+		$word = 'abcdefghijkmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ23456789';
+		$len = strlen($word);
+		for ($i = 0; $i < $password_len; $i++) {
+			$password .= $word[rand() % $len];
+		}
+		return $password;
 	}
 }
 new Submission;
