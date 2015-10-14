00b60429a3371c25908159115fc974e23843f545
Yen-chi Chen <mark86092@gmail.com>
2015-10-14 22:28:53 +0800

    admin part, fix #76

---
+++ admin/session/insert_speaker.php
@@ -0,0 +1,21 @@
+<?php
+namespace Admin;
+require_once('../../init.php');
+
+class Index extends \View {
+	public function post() {
+		$token = $_GET['token'];
+		$conn = new \Conn();
+		$auth = \Models\Auth::get($conn, $token);
+		if ($auth['scope'] == 'sudo') {
+			$session_id = $_POST['session_id'];
+			$value = $_POST['value'];
+			\Models\Sessions::insert_property($conn, $session_id, 'speaker', $value);
+			\Models\People::update_session_id($conn, $value, $session_id);
+			header('Location: edit.php?token=' . $token . '&session_id=' . $session_id);
+		} else {
+			throw new \UnauthorizedException();
+		}
+	}
+}
+new Index;
