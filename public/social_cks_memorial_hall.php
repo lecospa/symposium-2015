6bc45ee17b82a0b0501bbd03b933cd1319d5507e
Yen-chi Chen <mark86092@gmail.com>
2015-10-23 14:52:27 +0800

    第四階段修改 social*

--- public/social_cks_memorial_hall.php
+++ public/social_cks_memorial_hall.php
@@ -1,6 +1,6 @@
 <?php
-require_once('init.php');
-class Social extends View {
+require_once('../init.php');
+class Social extends \Controllers\Controller {
 	function get() {
 		$this->smarty->assign('scope', __CLASS__);
 		$this->smarty->display('social_cks_memorial_hall.html');
