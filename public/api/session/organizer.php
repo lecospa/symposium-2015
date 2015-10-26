caf8f39dc64b5f9e99b37b89a557a7e971deb7cd
Yen-chi Chen <mark86092@gmail.com>
2015-10-26 23:40:23 +0800

    add api

---
+++ public/api/session/organizer.php
@@ -0,0 +1,23 @@
+<?php
+namespace API\Session;
+require_once('../../../init.php');
+
+class OrganizerController extends \Controllers\APIController {
+	public function delete() {
+		$token = $_GET['token'];
+		$conn = new \Conn();
+		$auth = \Models\Auth::get($conn, $token);
+		if ($auth['scope'] == 'sudo') {
+			$id = $_POST['id'];
+			$session_id = $_POST['session_id'];
+			//\Models\Sessions::delete_property_by_id($conn, $id);
+			$this->json(array('status' => 'success'));
+		} else {
+			throw new \UnauthorizedException();
+		}
+	}
+	public function post() {
+		$this->json("hello");
+	}
+}
+new OrganizerController;
