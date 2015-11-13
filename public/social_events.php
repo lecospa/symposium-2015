06866df4204f0c8d35eb423f08e147f783e48fa5
Yen-chi Chen <mark86092@gmail.com>
2015-11-13 18:02:14 +0800

    social -> social_events

---
+++ public/social_events.php
@@ -0,0 +1,9 @@
+<?php
+require_once('../init.php');
+class SocialEvents extends \Controllers\Controller {
+	function get() {
+		$this->smarty->assign('scope', __CLASS__);
+		$this->smarty->display('social_events.html');
+	}
+}
+new SocialEvents;
