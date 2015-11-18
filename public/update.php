a0693ae27f78dced7aa6e82a338b0f0023b8ed66
Yen-chi Chen <mark86092@gmail.com>
2015-11-18 09:38:36 +0800

    update.php

--- public/update.php
+++ public/update.php
@@ -1,17 +1,15 @@
 <?php
 require_once('../init.php');
 
-class Update extends \Controllers\Controller {
+class Update extends \Controllers\APIController {
 	public function post() {
 		$conn = new \Conn();
 		$logging = new \Models\Logging($conn, $_SERVER);
 
-		header('Content-Type: text/plain');
 		exec('git pull origin master', $output);
-		foreach ($output as $line) {
-			echo $line."\n";
-		}
 		$logging->info('Update', json_encode(array('output' => $output)));
+
+		$this->json(array('output' => $output));
 	}
 }
 new Update;
