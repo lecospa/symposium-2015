eca25f176edd0e98d03143f1f46061465ff432a3
yktsai <yktsai@lecospa.ntu.edu.tw>
2015-12-01 14:36:35 +0800

    新增Plenary Session的地點 #157

--- public/admin/person/talk.php
+++ public/admin/person/talk.php
@@ -13,12 +13,13 @@ class Talk extends \Controllers\Controller {
 			$title = $_POST['title'];
 			$abstract = $_POST['abstract'];
 			$address_datetime = $_POST['address_datetime'];
+			$location = $_POST['location'];
 			$talk_time = $_POST['talk_time'];
 			$session = $_POST['session'];
 			$session_id = $_POST['session_id'];
 
-			$stmt = $conn->prepare("UPDATE `talks` SET `title`=?, `abstract`=?, `address_datetime`=?, `talk_time`=?, `session`=?, `session_id`=? WHERE `id`=?");
-			$stmt->execute(array($title, $abstract, $address_datetime, $talk_time, $session, $session_id, $talk_id));
+			$stmt = $conn->prepare("UPDATE `talks` SET `title`=?, `abstract`=?, `address_datetime`=?, `location`=?, `talk_time`=?, `session`=?, `session_id`=? WHERE `id`=?");
+			$stmt->execute(array($title, $abstract, $address_datetime, $location, $talk_time, $session, $session_id, $talk_id));
 			$logger->info('talk.update', json_encode(array('person_id' => $person_id, 'talk_id' => $talk_id, 'operator' => 'sudo')));
 
 			header('Location: ../person.php?token=' . $token . '&id=' . $person_id . '&mode=edit');
