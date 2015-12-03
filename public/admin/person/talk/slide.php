5352a29623917683133bd08a9f1269c78093cd7e
Yen-chi Chen <mark86092@gmail.com>
2015-12-03 20:14:04 +0800

    patch

--- public/admin/person/talk/slide.php
+++ public/admin/person/talk/slide.php
@@ -38,13 +38,16 @@ class Slide extends \Controllers\Controller {
 		$auth = \Models\Auth::get($conn, $token);
 		
 		if ($auth['scope'] == 'sudo') {
-			//找到person之後把slide刪掉
+			
 			$person_id = $_GET['person_id'];
-			\Models\People::delete_slide_file($conn, $person_id);
+			$talk_id = $_GET['talk_id'];
+			
+			$stmt = $conn->prepare("UPDATE `talks` SET `slide_file`= '' WHERE `id`=? AND `person_id`=?");
+			$stmt->execute(array($talk_id, $person_id));
 
 			// ???
 			$logger->info('person.delete', json_encode(array('id' => $person_id, 'operator' => 'sudo')));
-			header('Location: ' . TOP . '/admin/person.php?token='.$token);
+			header('Location: ' . TOP . '/admin/person.php?token='.$token . '&id=' . $person_id . '&mode=edit');
 		} else {
 			throw new \UnauthorizedException();
 		}
