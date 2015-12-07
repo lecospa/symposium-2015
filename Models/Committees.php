d46905613e073573af0fb3fc15be1fc5171de1ee
Yen-chi Chen <mark86092@gmail.com>
2015-12-07 11:21:47 +0800

    新增 comments

--- Models/Committees.php
+++ Models/Committees.php
@@ -7,6 +7,9 @@ namespace Models;
 
 class Committees {
 	private static $get_people_by_type_q = null;
+	/*
+	 * 取得某`type`的所有`people`
+	 */
 	static function get_people_by_type($conn, $type) {
 		if (is_null(self::$get_people_by_type_q)) {
 			self::$get_people_by_type_q = Query::prepare($conn, "SELECT `people`.* FROM `committee_person` LEFT JOIN `people` ON `committee_person`.`person_id` = `people`.`id` WHERE `committee_person`.`type`=? ORDER BY `people`.`last_name` ASC, `people`.`first_name`");
--- public/admin/person/talk.php
+++ public/admin/person/talk.php
@@ -6,21 +6,22 @@ class Talk extends \Controllers\AdminController {
 		$this->check('sudo');
 
 		$person_id = $_GET['person_id'];
-		$talk_id = $_GET['talk_id'];
+		$talk_id   = $_GET['talk_id'];
 
 		$conn = new \Conn();
 		$logger = new \Models\Logging($conn, $_SERVER);
 
-		$title = $_POST['title'];
-		$abstract = $_POST['abstract'];
+		$title            = $_POST['title'];
+		$abstract         = $_POST['abstract'];
 		$address_datetime = $_POST['address_datetime'];
-		$location = $_POST['location'];
-		$talk_time = $_POST['talk_time'];
-		$session = $_POST['session'];
-		$session_id = $_POST['session_id'];
+		$location         = $_POST['location'];
+		$talk_time        = $_POST['talk_time'];
+		$session          = $_POST['session'];
+		$session_id       = $_POST['session_id'];
 
 		$stmt = $conn->prepare("UPDATE `talks` SET `title`=?, `abstract`=?, `address_datetime`=?, `location`=?, `talk_time`=?, `session`=?, `session_id`=? WHERE `id`=?");
 		$stmt->execute(array($title, $abstract, $address_datetime, $location, $talk_time, $session, $session_id, $talk_id));
+		
 		$logger->info('talk.update', json_encode(array('person_id' => $person_id, 'talk_id' => $talk_id, 'operator' => 'sudo')));
 
 		header('Location: ../person.php?id=' . $person_id . '&mode=edit');
