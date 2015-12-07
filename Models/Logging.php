1423e1c1fb8b93c625efc222b2ad93f87a3dc3d2
Yen-chi Chen <mark86092@gmail.com>
2015-12-07 11:01:56 +0800

    update Logging

--- Models/Logging.php
+++ Models/Logging.php
@@ -2,10 +2,10 @@
 namespace Models;
 
 class Logging {
-	static function insert($conn, $event, $content, $remote_address, $forward_address) {
-		$stmt = $conn->prepare("INSERT INTO `logging` (`event`, `content`, `REMOTE_ADDR`, `HTTP_X_FORWARDED_FOR`) VALUES (?, ?, ?, ?)");
-		$stmt->execute(array($event, $content, $remote_address, $forward_address));
+	public function insert($event, $content, $remote_address, $forward_address) {
+		$this->insert_stmt->execute(array($event, $content, $remote_address, $forward_address));
 	}
+	private $insert_stmt;
 	private $remote_address;
 	private $forward_address;
 	private $conn;
@@ -15,8 +15,9 @@ class Logging {
 		if (!empty($_server['HTTP_X_FORWARDED_FOR'])) {
 			$this->forward_address = $server['HTTP_X_FORWARDED_FOR'];
 		}
+		$this->insert_stmt = $this->conn->prepare("INSERT INTO `logging` (`event`, `content`, `REMOTE_ADDR`, `HTTP_X_FORWARDED_FOR`) VALUES (?, ?, ?, ?)");
 	}
 	public function info($event, $content) {
-		Logging::insert($this->conn, $event, $content, $this->remote_address, $this->forward_address);
+		$this->insert($event, $content, $this->remote_address, $this->forward_address);
 	}
 }
