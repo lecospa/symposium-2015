ecd69f16d2a73ccd0c9e19e6056e1921fbeaa682
yktsai <yktsai@lecospa.ntu.edu.tw>
2015-12-10 20:42:45 +0800

    #186

--- public/admin/person/talk/slide_show.php
+++ public/admin/person/talk/slide_show.php
@@ -1,15 +1,15 @@
 <?php
 require_once('../../../../init.php');
 
-class Slide extends \Controllers\AdminController {
-	public function get() {
-		throw new \ForbiddenException();
-	}
+class SlideShow extends \Controllers\AdminController {
 	public function patch() {
 		$this->check('slide_patch');
 		$conn = new \Conn();
-
+		$person_id = $_GET['person_id'];
+		$slide_file_show = $_POST['slide_file_show'];
+		$stmt = $conn->prepare("UPDATE `talks` SET `slide_file_show`=? WHERE `id`=?");
+		$stmt->execute(array($slide_file_show, $person_id));
 	}
 }
 
-new Slide;
+new SlideShow;
