26f28f17e5828fe011072ee5b3fcf44fd2aec31a
Yen-chi Chen <mark86092@gmail.com>
2015-12-11 00:01:53 +0800

    Update talk.php

--- public/admin/person/talk.php
+++ public/admin/person/talk.php
@@ -2,15 +2,26 @@
 require_once('../../../init.php');
 
 class Talk extends \Controllers\AdminController {
+	/*
+	 * 更新某一場 talk
+	 * PATCH /admin/person/talk.php?person_id={}&talk_id={}
+	 */
 	public function patch() {
+		// 權限驗證
 		$this->check('sudo');
 
+		/*
+		 * 取得 person_id, talk_id
+		 */
 		$person_id = $_GET['person_id'];
 		$talk_id   = $_GET['talk_id'];
 
 		$conn = new \Conn();
 		$logger = new \Models\Logging($conn, $_SERVER);
 
+		/*
+		 * 需要更新的欄位名稱，透過POST
+		 */
 		$title            = $_POST['title'];
 		$abstract         = $_POST['abstract'];
 		$address_datetime = $_POST['address_datetime'];
@@ -18,12 +29,17 @@ class Talk extends \Controllers\AdminController {
 		$talk_time        = $_POST['talk_time'];
 		$session          = $_POST['session'];
 		$session_id       = $_POST['session_id'];
-
+		
+		/*
+		 * 資料庫更新
+		 */
 		$stmt = $conn->prepare("UPDATE `talks` SET `title`=?, `abstract`=?, `address_datetime`=?, `location`=?, `talk_time`=?, `session`=?, `session_id`=? WHERE `id`=?");
 		$stmt->execute(array($title, $abstract, $address_datetime, $location, $talk_time, $session, $session_id, $talk_id));
 		
+		// 紀錄事件發生
 		$logger->info('talk.update', json_encode(array('person_id' => $person_id, 'talk_id' => $talk_id, 'operator' => 'sudo')));
-
+		
+		// 返回至原本編輯的頁面
 		header('Location: ../person.php?id=' . $person_id . '&mode=edit');
 	}
 	public function delete() {
