82244364fa08bac6e74bd56923292fa13eb55a63
Yen-chi Chen <mark86092@gmail.com>
2016-01-11 11:44:38 +0800

    fix broken reference

--- public/admin/committee.php
+++ public/admin/committee.php
@@ -5,11 +5,11 @@ class Committee extends \Controllers\AdminController {
 	public function post() {
 		$this->check('committee_post');
 		$conn = new \Conn();
-		$type = $_POST['type'];
+		$committee_id = $_POST['committee_id'];
 		$person_id = $_POST['person_id'];
 
 		try {
-			\Models\Committees::insert_person($conn, $type, $person_id);
+			\Models\Committees::insert_person($conn, $committee_id, $person_id);
 		} catch (\PDOException $e) {
 			// do nothing
 		}
@@ -19,11 +19,11 @@ class Committee extends \Controllers\AdminController {
 	public function delete() {
 		$this->check('committee_delete');
 		$conn = new \Conn();
-		$type = $_GET['type'];
+		$committee_id = $_GET['committee_id'];
 		$person_id = $_GET['person_id'];
 
-		$stmt = $conn->prepare("DELETE FROM `committee_person` WHERE `type`=? AND `person_id`=?");
-		$stmt->execute(array($type, $person_id));
+		$stmt = $conn->prepare("DELETE FROM `committee_person` WHERE `committee_id`=? AND `person_id`=?");
+		$stmt->execute(array($committee_id, $person_id));
 
 		header('Location: ' . TOP . '/admin/committees.php');
 	}
